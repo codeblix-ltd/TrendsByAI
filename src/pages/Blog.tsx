12610699be919a6cd073e3a7c15517f9c466c91a
poorunkshamta17 <poorunkshamta17@gmail.com>
2025-10-28 21:34:17 +0400

    ewfew

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,6 +1,7 @@
 import { BlogIndex } from 'react-supabase-ssg-blog';
 import { supabase } from '@/lib/supabase';
 import BlogHeader from '@/components/ui/BlogHeader';
+import '../styles/blog-overrides.css';
 
 /**
  * Blog page - displays list of blog articles
@@ -8,24 +9,22 @@ import BlogHeader from '@/components/ui/BlogHeader';
 const Blog = () => {
   return (
     <div
-      className="min-h-screen bg-[#0D0F18] bg-cover bg-center p-4 sm:p-6 lg:p-8"
+      className="min-h-screen bg-[#0D0F18] bg-cover bg-center"
       style={{ backgroundImage: 'url(https://i.pinimg.com/originals/5d/56/99/5d56993d543ac6735b31ee3e4ac29d95.gif)' }}
     >
-      <div className="max-w-[1920px] mx-auto">
+      <div className="max-w-[1920px] mx-auto p-4 sm:p-6 lg:p-8">
         <BlogHeader />
 
         <main className="mt-8">
-          <div className="bg-white/95 backdrop-blur-sm rounded-3xl shadow-2xl p-6 sm:p-8">
-            <BlogIndex
-              supabaseClient={supabase}
-              basePath="/blog"
-              postsPerPage={10}
-              showExcerpt={true}
-              showAuthor={true}
-              showDate={true}
-              showTags={true}
-            />
-          </div>
+          <BlogIndex
+            supabaseClient={supabase}
+            basePath="/blog"
+            postsPerPage={10}
+            showExcerpt={true}
+            showAuthor={true}
+            showDate={true}
+            showTags={true}
+          />
         </main>
       </div>
     </div>
